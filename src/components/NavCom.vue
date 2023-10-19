656d72cdcf1f356c8eaf0d875543927e466c43ad
nimit0703 <nimit.190410107143@gmail.com>
2023-10-19 10:00:05 +0530

    nav structure design done

--- src/components/NavCom.vue
+++ src/components/NavCom.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="d-flex flex-column p-3">
+  <div class="">
     <div class="img-logo">
-      <img src="../assets/insta_logo.png.png" alt="" class="img-fluid" />
+      <img src="../assets/insta_logo.png.png" alt="" class="img-fluid insta-logo " />
     </div>
-    <div class="icons d-flex flex-column p-3">
+    <div class="icons d-flex flex-column ps-4">
       <div class="d-flex">
         <router-link to="home" class="icon-link">
           <i class="bi bi-house-door-fill"></i> <span class="text">Home</span>
@@ -11,17 +11,32 @@
       </div>
       <div class="d-flex">
         <router-link to="home" class="icon-link">
-          <i class="bi bi-house-door-fill"></i> <span class="text">Home</span>
+          <i class="bi bi-search"></i> <span class="text">Search</span>
         </router-link>
       </div>
       <div class="d-flex">
         <router-link to="home" class="icon-link">
-          <i class="bi bi-house-door-fill"></i> <span class="text">Home</span>
+          <i class="bi bi-compass"></i><span class="text">Explore</span>
         </router-link>
       </div>
       <div class="d-flex">
         <router-link to="home" class="icon-link">
-          <i class="bi bi-house-door-fill"></i> <span class="text">Home</span>
+          <i class="bi bi-chat-dots"></i> <span class="text">Message</span>
+        </router-link>
+      </div>
+      <div class="d-flex">
+        <router-link to="home" class="icon-link">
+          <i class="bi bi-heart"></i> <span class="text">Notification</span>
+        </router-link>
+      </div>
+      <div class="d-flex">
+        <router-link to="home" class="icon-link">
+          <i class="bi bi-plus-square"></i> <span class="text">create</span>
+        </router-link>
+      </div>
+      <div class="d-flex">
+        <router-link to="home" class="icon-link">
+          <img src="../assets/user-profile.jpeg" alt="" class="user-profile-nav"> <span class="text">Profile</span>
         </router-link>
       </div>
     </div>
@@ -38,10 +53,26 @@ export default {};
   align-items: center;
   justify-content: center;
   margin-left: 15px;
-  font-size: 1.5vw;
+  font-size: 1.7vw;
+  margin-bottom: 10px;
+}
+
+.icon-link i{
+  font-size: 2.5vw;
 }
 .text {
-  margin-top: 10px;
+  margin-top: 25px;
+  margin-left: 5px;
+}
+.insta-logo{
+width: 200px;
+margin: 0;
+}
+.user-profile-nav{
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  margin-top: 21px;
 }
 
 /* You can define more styles for the icon or text here */
