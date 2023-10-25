31c34e9ac11f32b34d1bdbfdd0b107b84af8da1f
nimit0703 <nimit.190410107143@gmail.com>
2023-10-25 12:54:19 +0530

    loader added successfully

---
+++ src/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="horizontal-loader"></div>
+</template>
+
+<script lang="ts">
+export default {
+  created(){
+    console.log("hello from loader")
+  },
+};
+</script>
+
+<style scoped>
+.horizontal-loader {
+  position: absolute;
+  width: 100%;
+  height: 5px;
+  background: linear-gradient(to right, rgb(250, 115, 217), #8261f9, #21004a);
+  animation: loadani infinite 500ms;
+}
+@keyframes loadani {
+  30% {
+    background: linear-gradient(to right, #21004a, rgb(250, 115, 217), #8261f9);
+  }
+  60% {
+    background: linear-gradient(to right, #21004a, rgb(250, 115, 217), #21004a);
+  }
+  
+}
+</style>
