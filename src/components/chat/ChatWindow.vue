756bf0d0623f8e1773fb2ddbfb237e002e72caf7
nimit0703 <nimit.190410107143@gmail.com>
2023-11-18 18:13:21 +0530

    made changes but not working

---
+++ src/components/chat/ChatWindow.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <div v-for="message in messages" :key="message.id">
+        <strong>{{ message.sender }}</strong>: {{ message.content }}
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    props: ['messages'],
+  };
+  </script>
+  
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import store from "./stores/store";
 import _ from "lodash-es";
 import './assets/main.css'
+import socket from './socket';
 
 const app = createApp(App);
 
 // Use the router
 app.use(router);
 app.use(store);
+
 app.mount("#app"); // Mount the app after data is loaded
