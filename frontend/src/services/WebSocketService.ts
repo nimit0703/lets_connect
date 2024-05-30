c4960476c0b37a4fcd568ddb2e70e2f9e58dfa33
nimit0703 <nimit.190410107143@gmail.com>
2024-05-30 12:14:58 +0530

    removed websockets

--- frontend/src/services/WebSocketService.ts
+++ frontend/src/services/WebSocketService.ts
@@ -1,29 +1,29 @@
-// import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+// // import SockJS from 'sockjs-client';
+// // import Stomp from 'stompjs';
 
-import SockJS from 'sockjs-client/dist/sockjs'; // Note the explicit path to sockjs
+// // import SockJS from 'sockjs-client/dist/sockjs'; // Note the explicit path to sockjs
 
-// Rest of the code
+// // // Rest of the code
 
-export class WebSocketService {
-  private stompClient: Stomp.Client;
+// // export class WebSocketService {
+// //   private stompClient: Stomp.Client;
 
-  constructor() {
-    const socket = new SockJS('http://localhost:8080/ws');
-    this.stompClient = Stomp.over(socket);
-    this.stompClient.connect({}, () => {
-      console.log('WebSocket connected');
-    });
-  }
+// //   constructor() {
+// //     const socket = new SockJS('http://localhost:8080/ws');
+// //     this.stompClient = Stomp.over(socket);
+// //     this.stompClient.connect({}, () => {
+// //       console.log('WebSocket connected');
+// //     });
+// //   }
 
-  sendMessage(destination: string, message: Message) {
-    this.stompClient.send(destination, {}, JSON.stringify(message));
-  }
+// //   sendMessage(destination: string, message: Message) {
+// //     this.stompClient.send(destination, {}, JSON.stringify(message));
+// //   }
 
-  subscribe(destination: string, callback: (message: Message) => void) {
-    this.stompClient.subscribe(destination, (response) => {
-      const message: Message = JSON.parse(response.body);
-      callback(message);
-    });
-  }
-}
+// //   subscribe(destination: string, callback: (message: Message) => void) {
+// //     this.stompClient.subscribe(destination, (response) => {
+// //       const message: Message = JSON.parse(response.body);
+// //       callback(message);
+// //     });
+// //   }
+// }
