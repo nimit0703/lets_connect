cf60a6e2ea4dc673fb26110a80f4836e5fe2ac83
nimit0703 <nimit.190410107143@gmail.com>
2024-05-31 16:16:45 +0530

    resolved path issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,13 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import VueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
 export default defineConfig({
   plugins: [Vue(), VueJsx()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
